94f9124aeef030d6499dc62417a1b2d3ac9ece90
TEUN\teuna <teunaarts@live.nl>
2018-04-23 14:20:40 +0200

    initial version

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const application = express();
+const port = 8080;
+
+application.all('*', (request, response, next) =>{
+    console.log(request.url);
+    next();
+});
+
+application.listen(port, () => {
+    console.log('magic happens at port ' + port);
+});
+
+module.exports = application;
+
